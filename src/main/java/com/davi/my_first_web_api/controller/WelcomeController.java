505d767eeec4225bf7443f14eac03efd51d312ec
DaviCarvalhoo <davicarvalhotech@gmail.com>
2024-07-16 21:12:31 -0300

    primeiro controller

---
+++ src/main/java/com/davi/my_first_web_api/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.davi.my_first_web_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome(){
+        return "Bem-vindo! a minha api";
+    }
+}
